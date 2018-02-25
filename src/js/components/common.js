ac62ae71f6ba301633b5733e9b58a5304ccdd0ff
deniskliatskii <deniskldesign@gmail.com>
2018-02-25 23:11:39 +0200

    jquery

--- src/js/components/common.js
+++ src/js/components/common.js
@@ -5,14 +5,45 @@ $(function () {
 * GALLERY
 *
 */
+var imagePos = 0; 
+var widthImages = $('.gallery-min').outerWidth();
+var galleryImagesLenght = $('.gallery-min').length;
+var galleryWrap = $('.gallery-tumbs').width();
+var movPos = widthImages * galleryImagesLenght - galleryWrap;
+
+    // Left move NEXT
+    $('.arrow-next__gallery').click(function () {
+        $('.arrow-prew__gallery').addClass('activ-arrow');
+        imagePos -= widthImages;
+        $('.gallery-item').css('position', 'relative');
+
+        if (imagePos < - movPos) {
+            imagePos = 0;
+        }
+        $('.gallery-item').css( 'left', imagePos );
+    });
+
+    // Right move PREW
+    $('.arrow-prew__gallery').click(function () {
+        if (imagePos < 0) {
+            imagePos += widthImages;
+            $('.gallery-item').css('left', imagePos);
+        }
+        else {
+            $('.arrow-prew__gallery').removeClass('activ-arrow');
+        }
+    });
+
+
 
     //Gallery full images
     $('.gallery-min a').click(function (event) {
         event.preventDefault();
         var imgAtribut = $(this).attr('href');
         $('.gallery-prewiew img').attr( { src: imgAtribut } );
-    })
+    });
 
+    // Modal window end overlay for gallery images
     $('.expand-btn').click(function () {
         var modalWindowContent = $('.gallery-prewiew img').attr('src');
         $('body').append('<img class="modal-images">');
@@ -23,8 +54,8 @@ $(function () {
         $('.modal-overlay').click(function () {
             $('.modal-overlay').remove();
             $('.modal-images').remove();
-        })
-    })
+        });
+    });
 
 /*
 *
@@ -37,13 +68,13 @@ $(function () {
 
     $('.quantity-plus').click(function (event) {
         $('.quantity-count').text(++numCount);
-    })
+    });
 
     $('.quantity-min').click(function (event) {
         if (numCount > 1) {
             $('.quantity-count').text(--numCount);
         }
-    })
+    });
 
 
 /*
@@ -53,6 +84,7 @@ $(function () {
 */
 
     $('.tabs-content__wrap > div:not(:first)').hide();
+
     $('.tabs__list a').click(function (event) {
         event.preventDefault();
 
@@ -64,7 +96,7 @@ $(function () {
         $('.tabs-content__wrap > div').hide();
         $(tabShow).show();
 
-    })
+    });
 
     
 /*
@@ -87,7 +119,7 @@ $(function () {
         } else {
             $('.btn-up').removeClass('btn-up__active');
         }
-    })
+    });
  
 
 });
